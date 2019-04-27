cfc5ff5128cf1ea98f46d9a2e019762c53f03ea8
Elijah Friedrich <Elijah Friedrich@172.31.127.131>
2019-04-27 17:31:43 -0400

    Completed spend_points()

--- src/main/java/edu/smith/cs/csc212/fp/Main.java
+++ src/main/java/edu/smith/cs/csc212/fp/Main.java
@@ -20,7 +20,7 @@ public class Main {
 //		  Move to character creation.
 		  if (input.equals("yes")) {
 			  System.out.println("Starting... ");
-			  character_creation();
+			  Player.character_creation();
 			  }
 //		  Quit.
 		  else if (input.equals("no")) {
@@ -34,97 +34,10 @@ public class Main {
 		  }
 	  }
   
-  private static void character_creation() {
-	  System.out.println("___________________");
-	  System.out.println("|CHARACER CREATION|");
-	  System.out.println("Do you want to build your own chracter from scratch?");
-	  String input = "";
-	  
-	  while (!input.equals("yes")) {
-		  Scanner scanner = new Scanner(System.in);
-		  input = scanner.next();
-//		  System.out.println("This is the current input: " + input);
-//		  System.out.println(player.print_details());
-		  
-//		  Custom character.
-		  if (input.equals("yes")) {
-//			  Build blank character, modify by user input.
-			  Player player = new Player("TestName", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-			  System.out.println("What's your name? ");
-			  String name = "";
-			  name = scanner.next();
-			  player.name = name;
-			  System.out.println("Your name is: " + player.name + ". ");
-			  
-			  int age = 0;
-			  System.out.println("\nWhat's your age? ");
-//			  While age is not between 10 and 90.
-			  
-//			  Scanner scan = new Scanner(System.in);
 
-			  while (age < 10 || age > 90) {
-//				  When we don't have an int, we're going to stay here until we do.
-				  while(!scanner.hasNextInt()) {
-				      scanner.next();
-				      System.out.println("Please type a valid number. ");
-				      }
-//				  Soon as we leave the loop with an int, we put it into the age variable.
-				  age = scanner.nextInt();
-//				  now we examine the number a little.
-				  if (age < 10) {
-					  System.out.println("You're too young! ");
-					  continue;
-					  }
-				  else if (age > 90) {
-					  System.out.println("You're too old! ");
-					  continue;
-					  }
-//				  We accept the number!
-				  else if (age >= 10 && age <= 90) {
-					  player.age = age;
-					  System.out.println("\nYour age is: " + player.age + ". ");
-					  
-		//			  Set base stats and free points.
-					  player.endurance += 5;
-					  player.strength += 5;
-					  player.stat_points += 5;
-					  player.next_level_exp = 100;
-		//			  spend skill points now.
-					  player.spend_points(player);
-		//			  calc stat output based on inputs.
-					  player.calculate_stats(player);
-					  }
-				  }
-			  }
-//		  Default character.
-		  else if (input.equals("no")) {
-			  Player player = new Player(
-					  "DefaultDan", // name
-					  10, // age
-					  45, // hp
-					  10, // max_hp
-					  5, // skill points
-					  5, // endurance
-					  5, //strength
-					  1, // armor value
-					  1, // damage value, base 1.
-					  1, // level, base 1. 
-					  0, // xp
-					  100, // next lvl xp
-					  0); // money
-
-			  System.out.println(player.print_details());
-			  break;
-			  }
-//		  Invalid input.
-		  else {
-			  System.out.println("Please type 'yes' or 'no'");
-			  }
-		  }
-	  }
   
 //  takes in arg for each variable needed to build player?
-private static void calculate_character() {
+public static void calculate_character() {
 //	int max_hp,
 //	int stat_points,
 //	int endurance,
@@ -146,7 +59,7 @@ private static void calculate_character() {
 
 //does method take in arguments?
 //should know current zone so that it knows where player is.
-private static void call_player_methods() {
+public static void call_player_methods() {
 //	we will call this in zones to be able to process input w/o repeating code?
 //	with the graph/zone/nodes we can just node.exit/next sort of thing?
 //	can refer to node.name (place.description?) 
@@ -158,28 +71,7 @@ private static void call_player_methods() {
 
 public static void main(String[] args) {
 	  play_game();
-//	  call_player_methods();
-	  
-//	  Ideally this would work and be easy but it works specifically for the console, it throws an error in an IDE.
-//	  https://www.admfactory.com/how-to-get-input-from-java-console/
-	  //    String testingUserInput = System.console().readLine();
-//    System.out.println(testingUserInput);
-    
-	  
-////	  How we read user input
-//	  Scanner scanner = new Scanner(System.in);
-////	  remember to close scanner later
-//	  while (true) {
-//		  System.out.println("Make character? \n ");
-//		  String text = scanner.nextLine();
-//		  if (text.equalsIgnoreCase("yes")){
-//			  System.out.println(player.print_details());
-////			  player.print_details();
-////			  System.out.println("test!");
-//			  
-//			  break;
-//		  }
-//	  }
+
 	  }
   }
 
--- src/main/java/edu/smith/cs/csc212/fp/Player.java
+++ src/main/java/edu/smith/cs/csc212/fp/Player.java
@@ -1,31 +1,26 @@
 package edu.smith.cs.csc212.fp;
 
+import java.util.Scanner;
+
 public class Player {
 	
 //	personal stuff
 	String name;
 	int age;
-	
 	int hp;
 	int max_hp;
-	
 // 	stats
 	int stat_points;
 	int endurance;
 	int strength;
-	
 	int defense_value;
 	int damage_value;
-	
 //	level
 	int level;
 	int current_exp;
 	int next_level_exp;
-	
 	int currency;
 
-	
-	
 	
 	public Player(String name,
 			int hp,
@@ -58,19 +53,90 @@ public class Player {
 	
 //	These points can be spent to increase strength or endurance.
 	public void spend_points(Player player) {
-
 		System.out.println("______________________");
 		System.out.println("|SPENDING STAT POINTS|");
 		System.out.println("Endurance: " + player.endurance + ". ");
 		System.out.println("Strength: " + player.strength + ". ");
+		System.out.println("\nUnspent points: " + player.stat_points + ". ");
 		
+//		Valid amount of points.
 		if (player.stat_points > 0) {
-			System.out.println("Unspent points: " + player.stat_points + ". ");
-			
-//		would be cool to add speed which increases chances of getting a second action in a row sort of thing.	
-			
+			String input = "";
+			int spendInput = 0;
+
+			while (!input.equals("endurance") || !input.equals("strength") || !input.equals("cancel")) {
+				System.out.println("Options: 'endurance' or 'strength' or 'cancel' ");
+				Scanner scanner = new Scanner(System.in);
+				input = scanner.next();
+				
+				if (input.equals("endurance")) {
+					System.out.println("\nIncrease endurance by: ");
+					while (spendInput <= 0 && !scanner.hasNextInt() ){
+						scanner.next();
+						System.out.println("Please type a valid number. ");
+						}
+					spendInput = scanner.nextInt();
+//					if there is a valid amount of points entered
+					if (spendInput <= player.stat_points) {
+						System.out.println("player endurance before increase: " + player.endurance);
+						System.out.println("player stat_points before increase: " + player.stat_points);
+						System.out.println("spendInput current value: " + spendInput);
+						player.endurance += spendInput;
+						player.stat_points -= spendInput;
+
+						System.out.println("player endurance after increase: " + player.endurance);
+						System.out.println("player stat_points after increase: " + player.stat_points);
+//						Compute the stats (increase max hp, etc.)
+						calculate_stats(player);
+						break;
+						}
+//						if they want to spend more points than they have available.
+					else if (spendInput > player.stat_points) {
+						System.out.println("You don't have " + spendInput + " points available to spend. ");
+						System.out.println("Current available points: " + player.stat_points + ".\n ");
+						}
+					}
+				else if (input.equals("strength")) {
+					System.out.println("\nIncrease strength by: ");
+					while (spendInput <= 0 && !scanner.hasNextInt() ){
+						scanner.next();
+						System.out.println("Please type a valid number. ");
+						}
+					spendInput = scanner.nextInt();
+//					if there is a valid amount of points entered
+					if (spendInput <= player.stat_points) {
+						System.out.println("player strength before increase: " + player.strength);
+						System.out.println("player stat_points before increase: " + player.stat_points);
+						System.out.println("spendInput current value: " + spendInput);
+						player.strength += spendInput;
+						player.stat_points -= spendInput;
+
+						System.out.println("player strength after increase: " + player.strength);
+						System.out.println("player stat_points after increase: " + player.stat_points);
+//						Compute the stats (increase max hp, etc.)
+						calculate_stats(player);
+						break;
+						}
+//						if they want to spend more points than they have available.
+					else if (spendInput > player.stat_points) {
+						System.out.println("You don't have " + spendInput + " points available to spend. ");
+						System.out.println("Current available points: " + player.stat_points + ".\n ");
+						}
+				}
+				else if (input.equals("cancel")) {
+					System.out.println("Canceling spend points. ");
+					break;
+				}
+			}
 		}
-	}
+//		Invalid amount of points.
+		else if (player.stat_points <= 0) {
+			System.out.println("You don't have enough points available to spend.\n ");
+			}
+		}
+	
+	
+	
 	
 //	don't think this is needed... can just call level_up even for initial creation?
 	public void calculate_stats(Player player) {	
@@ -116,6 +182,97 @@ public class Player {
 				+ "\nNext Level: " + next_level_exp + "."
 				+ "\nPlaceholder for inventory/money: " + currency + ".");
 		}
+	
+	
+	
+	  public static void character_creation() {
+		  System.out.println("___________________");
+		  System.out.println("|CHARACER CREATION|");
+		  System.out.println("Do you want to build your own chracter from scratch?");
+		  String input = "";
+		  
+		  while (!input.equals("yes")) {
+			  Scanner scanner = new Scanner(System.in);
+			  input = scanner.next();
+//			  System.out.println("This is the current input: " + input);
+//			  System.out.println(player.print_details());
+			  
+//			  Custom character.
+			  if (input.equals("yes")) {
+//				  Build blank character, modify by user input.
+				  Player player = new Player("TestName", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+				  System.out.println("What's your name? ");
+				  String name = "";
+				  name = scanner.next();
+				  player.name = name;
+				  System.out.println("Your name is: " + player.name + ". ");
+				  
+				  int age = 0;
+				  System.out.println("\nWhat's your age? ");
+//				  While age is not between 10 and 90.
+				  
+//				  Scanner scan = new Scanner(System.in);
+
+				  while (age < 10 || age > 90) {
+//					  When we don't have an int, we're going to stay here until we do.
+					  while(!scanner.hasNextInt()) {
+					      scanner.next();
+					      System.out.println("Please type a valid number. ");
+					      }
+//					  Soon as we leave the loop with an int, we put it into the age variable.
+					  age = scanner.nextInt();
+//					  now we examine the number a little.
+					  if (age < 10) {
+						  System.out.println("You're too young! ");
+						  continue;
+						  }
+					  else if (age > 90) {
+						  System.out.println("You're too old! ");
+						  continue;
+						  }
+//					  We accept the number!
+					  else if (age >= 10 && age <= 90) {
+						  player.age = age;
+						  System.out.println("\nYour age is: " + player.age + ". ");
+						  
+			//			  Set base stats and free points.
+						  player.endurance += 5;
+						  player.strength += 5;
+						  player.stat_points += 5;
+						  player.next_level_exp = 100;
+			//			  spend skill points now.
+						  player.spend_points(player);
+			//			  calc stat output based on inputs.
+						  player.calculate_stats(player);
+						  }
+					  }
+				  }
+//			  Default character.
+			  else if (input.equals("no")) {
+				  Player player = new Player(
+						  "DefaultDan", // name
+						  10, // age
+						  45, // hp
+						  10, // max_hp
+						  5, // skill points
+						  5, // endurance
+						  5, //strength
+						  1, // armor value
+						  1, // damage value, base 1.
+						  1, // level, base 1. 
+						  0, // xp
+						  100, // next lvl xp
+						  0); // money
+
+				  System.out.println(player.print_details());
+				  break;
+				  }
+//			  Invalid input.
+			  else {
+				  System.out.println("Please type 'yes' or 'no'");
+				  }
+			  }
+		  }
 	
 //	handle gold/items in player class or separate class?
 //	think it makes sense to do in here because I could do the same thing for enemies.
